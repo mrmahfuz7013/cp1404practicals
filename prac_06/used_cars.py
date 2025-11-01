748b6e3a746fdba82483f889f39dfdd6ea832c11
Mahfuzur Rahman <mrmahfuz7013@gmail.com>
2025-11-02 09:58:13 +1000

    Add Used_cars

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,7 +1,5 @@
 """
 CP1404 Practical - Client code to use the Car class.
-Note that the import has a folder (module) in it.
-This is why we name our folders with no spaces or capitals, as valid module names.
 """
 
 from prac_06.car import Car
