e0785158380770617e2bd70b3da2b50fa3e03ca4
Mahfuzur Rahman <mrmahfuz7013@gmail.com>
2025-11-21 08:47:03 +1000

    Add Taxi subclass (with class variable) and implement UnreliableCar with reliability-based driving

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Practical
+UnreliableCar class, derived from Car
+"""
+
+from random import randint
+from car import Car
+
+
+class UnreliableCar(Car):
+    """An unreliable version of a car."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car, only sometimes, based on reliability.
+
+        Generate a random number between 1 and 100 and only drive if it is
+        less than the car's reliability. Always return the distance driven.
+        """
+        random_number = randint(1, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
